d562ee47ad3acaee6ed6f97374f2661231a1c4c4
Duarte Escairo <a106936@alunos.uminho.pt>
2025-12-02 22:41:43 +0000

    Cache e GestorLogins

---
+++ registoDeEventos/src/Cache.java
@@ -0,0 +1,77 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class Cache<K,V>{
+     private final int capacidade;
+     private final Map<K,V> map;
+     private final ReentrantLock lock = new ReentrantLock();
+
+     public Cache(int capacidade){
+          this.capacidade = capacidade;
+          this.map = new LinkedHashMap<>(capacidade, 1.0f, true){
+               protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                    // Remove o elemento mais antigo quando exceder a capacidade
+                    return size() > Cache.this.capacidade;
+               }
+          };
+     }
+
+     public V get(K key){
+          lock.lock();
+          try{
+               return map.get(key);
+          }finally{
+               lock.unlock();
+          }
+     }
+
+     public void put(K key, V value){
+          lock.lock();
+          try{
+               map.put(key, value);
+          }finally{
+               lock.unlock();
+          }
+     }
+
+     public int size(){
+          lock.lock();
+          try{
+               return map.size();
+          }finally{
+               lock.unlock();
+          }
+     }
+
+     public boolean containsKey(K key){
+          lock.lock();
+          try{
+               return map.containsKey(key);
+          }finally{
+               lock.unlock();
+          }
+     }
+
+     public boolean remove(K key,V value){
+          lock.lock();
+          try {
+               return map.remove(key, value);
+          } finally {
+               lock.unlock();
+          }
+     }
+
+     // Para depuração: mostra o estado da cache
+     @Override
+     public String toString() {
+          lock.lock();
+          try {
+               return map.toString();
+          } finally {
+               lock.unlock();
+          }
+     }
+
+}
---
+++ registoDeEventos/src/GestorLogins.java
@@ -0,0 +1,52 @@
+
+
+
+public class GestorLogins {
+     Cache<String,String> cache; //<USER,PASSWORD>
+     BDLogin users;
+
+     public GestorLogins(int capacidade){
+          this.cache = new Cache<>(capacidade);
+          this.users = new BDLogin("files/users.txt");
+     }
+
+     public boolean registar(String username,String password) {
+          if (!(username != null && password != null)) {
+               return false;
+          }
+          if (!cache.containsKey(username) && !users.containsUser(username)){
+               users.add(username, password);
+               cache.put(username, password);
+               return true;
+          }
+          return false;
+     }
+     
+     public boolean autenticar(String username,String password) {
+          if (cache.containsKey(username)){
+               if (cache.get(username).equals(password)){
+                    return true;
+               }
+          } else if (users.containsUser(username)){
+               if (users.get(username).equals(password)) {
+                    cache.put(username, password);
+                    return true;
+               }
+          } 
+          return false;
+     }
+
+     public boolean apagar(String username, String password) {
+          if (cache.containsKey(username)){
+               if (cache.get(username).equals(password)){
+                    cache.remove(username, password);
+               }
+          } else if (users.containsUser(username)){
+               if (users.get(username).equals(password)) {
+                    users.remove(username,password);
+               }
+          } 
+          return false;
+     }
+
+}
