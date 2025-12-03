36db3e69d511477f27d428e075be429d06cf26d7
Duarte Escairo <a106936@alunos.uminho.pt>
2025-12-03 22:38:51 +0000

    Client inicializado e buffers respetivos também.

---
+++ registoDeEventos/src/client/Client.java
@@ -0,0 +1,32 @@
+package client;
+
+import java.io.IOException;
+import java.net.Socket;
+import structs.ClientBuffer;
+
+public class Client {
+     private ClientBuffer bufferOut;  // para enviar ao servidor
+     private ClientBuffer bufferIn;   // para guardar respostas
+
+     public static void main(String[] args) {
+          ClientBuffer bufferOut = new ClientBuffer();
+          ClientBuffer bufferIn = new ClientBuffer();
+
+          try {
+               int porta = Integer.parseInt(args[0]);
+               Socket socket = new Socket("localhost", porta);
+
+               // Lançar thread writer
+               new Thread(new ClientWriter(socket, bufferIn)).start();
+
+               // Lançar thread reader
+               new Thread(new ClientReader(socket, bufferIn)).start();
+
+          } catch (IOException e) {
+               e.printStackTrace();
+          } catch(Exception e) {
+               e.printStackTrace();
+          }
+     }
+
+}
