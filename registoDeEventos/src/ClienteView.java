3893e6fe228f746e67f85ce611c69ba7d67b0b69
Duarte Escairo <a106936@alunos.uminho.pt>
2025-12-04 17:00:40 +0000

    Client View + Client com métodos a implementar

---
+++ registoDeEventos/src/ClienteView.java
@@ -0,0 +1,72 @@
+import menu.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClienteView {
+
+    private final Cliente cliente;
+
+    public ClienteView(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void iniciar() {
+        mostrarMenuConexao();
+    }
+
+    // ------------------------ MENU DE CONEXÃO ------------------------
+
+    private void mostrarMenuConexao() {
+
+        List<MenuOpcao> opcoes = Arrays.asList(
+            new MenuOpcao("Conectar ao Servidor", () -> {
+                cliente.conectar();
+                if (cliente.isConectado()) {
+                    System.out.println("Ligação estabelecida!");
+                    mostrarMenuPrincipal();
+                }
+            })
+        );
+
+        Menu menu = new Menu(opcoes);
+        menu.run();
+    }
+
+    // ------------------------ MENU PRINCIPAL -------------------------
+
+    private void mostrarMenuPrincipal() {
+
+        List<MenuOpcao> opcoes = Arrays.asList(
+
+            new MenuOpcao("Enviar Evento",
+                () -> cliente.enviarEvento()
+            ),
+
+            new MenuOpcao("Enviar Query 1",
+                () -> cliente.enviarQuery1()
+            ),
+
+            new MenuOpcao("Enviar Query 2",
+                () -> cliente.enviarQuery2()
+            ),
+
+            new MenuOpcao("Enviar Query 3",
+                () -> cliente.enviarQuery3()
+            ),
+
+            new MenuOpcao("Consultar Respostas",
+                () -> cliente.consultarRespostas()
+            ),
+
+            new MenuOpcao("Desconectar",
+                () -> {
+                    cliente.desconectar();
+                    mostrarMenuConexao();
+                }
+            )
+        );
+
+        Menu menu = new Menu(opcoes);
+        menu.run();
+    }
+}
