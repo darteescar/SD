18f0f3832c742b331e6c7b2b7a3dca767ce12208
Duarte Escairo <a106936@alunos.uminho.pt>
2025-12-05 23:11:57 +0000

    GestorSeries

--- registoDeEventos/src/structs/GestorSeries.java
+++ registoDeEventos/src/structs/GestorSeries.java
@@ -13,10 +13,42 @@ public class GestorSeries {
      }
 
      public boolean add(Serie serie) {
-          return true;
+          if (serie == null || serie.getData() == null) {
+               return false; // campos inválidos
+          }
+          if (!bd.containsKey(serie.getData())){ // se não está na BD, insere na BD e na Cache
+               bd.put(serie.getData(), serie);
+               cache.put(serie.getData(), serie);
+               return true;
+          }
+          return false; // série existe
      }
 
      public boolean remove(String dia) {
-          return true;
+          if (cache.containsKey(dia)){ // se está na Cache
+               cache.remove(dia); // remove da cache
+               bd.remove(dia); // remove da BD
+               return true;
+          } else if (bd.containsKey(dia)){ // se está na BD
+               bd.remove(dia); // remove da BD
+               return true;
+          }
+          return false; // série não existe
      }
+
+     public boolean contains(String dia){    
+          return cache.containsKey(dia) || bd.containsKey(dia); 
+     }
+
+     public Serie get(String dia) {
+          if (cache.containsKey(dia)){ // MISS - se está na cache
+               return cache.get(dia);
+          } else if (bd.containsKey(dia)){ // MISS - se está na BD
+               Serie s = bd.get(dia);
+               cache.put(dia, s);
+               return s;
+          } 
+          return null; // série não existe
+     }
+
 }
