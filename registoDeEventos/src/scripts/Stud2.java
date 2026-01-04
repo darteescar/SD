1b2da7be92fd7a1e428a937619792d9ba821d415
Duarte Escairo <a106936@alunos.uminho.pt>
2026-01-04 23:35:16 +0000

    Stud2

--- registoDeEventos/src/scripts/Stud2.java
+++ registoDeEventos/src/scripts/Stud2.java
@@ -13,16 +13,36 @@ import java.util.concurrent.locks.ReentrantLock;
 import utils.structs.client.NotificacaoListener;
 import utils.workers.client.Demultiplexer;
 
+/** Classe que realiza a comunicação com o servidor e gere o envio e receção de mensagens nos scripts */
 public class Stud2 implements AutoCloseable {
 
+    /** Socket para comunicação com o servidor */
     private final Socket socket;
+
+    /** Thread Demultiplexer para gerir a comunicação assíncrona */
     private final Demultiplexer demu;
+
+    /** Contador para identificar mensagens */
     private int idMensagem = 0;
+
+    /** Lista para armazenar respostas recebidas */
     private final List<String> replies;
+
+    /** Mapa para armazenar timestamps de mensagens por tipo */
     private final Map<TipoMsg, List<Long>> messageTimestamps;
+
+    /** Listener para notificações recebidas */
     private NotificacaoListener listener;
+
+    /** Lock para sincronização de acesso a recursos compartilhados (lista de respostas e timestamps) */
     private ReentrantLock lock = new ReentrantLock();
 
+    /** 
+     * Construtor da classe Stud2
+     * 
+     * @throws IOException se ocorrer um erro ao conectar ao servidor
+     * @return Uma nova instância de Stud2
+     */
     public Stud2() {
         try {
             this.socket = new Socket("localhost", 12345);
@@ -45,27 +65,51 @@ public class Stud2 implements AutoCloseable {
         }
     }
 
+    /** 
+     * Define o listener para notificações recebidas
+     * 
+     * @param listener O listener a ser definido
+     */
     public void setNotificacaoListener(NotificacaoListener listener){
         this.listener = listener;
     }
 
+    /** 
+     * Inicia as threads que gerem a comunicação com o servidor (Demultiplexer)
+     * 
+     * @return void
+     */
     public void start(){
         this.demu.start();
     }
 
+    /** 
+     * Fecha a comunicação com o servidor e termina o Demultiplexer
+     * 
+     * @throws IOException se ocorrer um erro ao fechar o socket ou demultiplexer
+     */
     @Override
     public void close() throws IOException {
         this.demu.close();
         this.socket.close();
     }
 
-    
+    /** 
+     * Envia uma mensagem ao servidor utilizando uma thread separada (Sender2)
+     * 
+     * @param mensagem A mensagem a ser enviada
+     */
     public void send(Mensagem mensagem){
         Thread sender = new Thread(new Sender2(this.demu, mensagem, replies, listener, lock, messageTimestamps));
         sender.start();
     }
 
-    
+    /** 
+     * Envia uma mensagem ao servidor e aguarda a resposta (sincronamente)
+     * 
+     * @param mensagem A mensagem a ser enviada
+     * @return true se a resposta for "true", false caso contrário
+     */
     private boolean sendAndWait(Mensagem mensagem) {
         boolean result = false;
         try {
@@ -96,6 +140,15 @@ public class Stud2 implements AutoCloseable {
         return result;
     }
 
+    /** 
+     * Envia uma mensagem de login ao servidor e aguarda a resposta
+     * 
+     * @param tipo O tipo de mensagem (deve ser LOGIN)
+     * @param username O nome de usuário
+     * @param password A senha do usuário
+     * @return true se o login for bem-sucedido, false caso contrário
+     * @throws IOException se ocorrer um erro ao enviar a mensagem
+     */
     public boolean sendLOGIN(TipoMsg tipo, String username, String password) throws IOException{
         Login login = new Login(username, password);
         byte[] bytes = login.serialize();
@@ -103,6 +156,15 @@ public class Stud2 implements AutoCloseable {
         return sendAndWait(mensagem);
     }
 
+    /** 
+     * Envia uma mensagem de registo de evento ao servidor e lança uma thread para aguardar a resposta
+     * 
+     * @param tipo O tipo de mensagem (deve ser REGISTA_LOGIN)
+     * @param username O nome de usuário
+     * @param password A senha do usuário
+     * @return true se o registo for bem-sucedido, false caso contrário
+     * @throws IOException se ocorrer um erro ao enviar a mensagem
+     */
     public void sendEVENTO_INVALIDO(TipoMsg tipo) throws IOException {
         String produtoInvalido = "";   // Produto vazio
         int quantidadeInvalida = -10;  // Quantidade negativa
@@ -116,37 +178,82 @@ public class Stud2 implements AutoCloseable {
         this.send(mensagem);
     }
 
+    /** 
+     * Envia uma mensagem de registo de evento ao servidor e lança uma thread para aguardar a resposta
+     * 
+     * @param tipo O tipo de mensagem
+     * @param produto O nome do produto
+     * @param quantidade A quantidade do produto
+     * @param preco O preço do produto
+     * @throws IOException se ocorrer um erro ao enviar a mensagem
+     */
     public void sendEVENTO(TipoMsg tipo, String produto, int quantidade, double preco) throws IOException{
         Evento evento = new Evento(produto, quantidade, preco);
         Mensagem mensagem = new Mensagem(idMensagem++, tipo, evento.serialize());
         this.send(mensagem);
     }
 
+    /** 
+     * Envia uma mensagem de agregação ao servidor e lança uma thread para aguardar a resposta
+     * 
+     * @param tipo O tipo de mensagem
+     * @param produto O nome do produto
+     * @param dias O número de dias para agregação
+     * @throws IOException se ocorrer um erro ao enviar a mensagem
+     */
     public void sendAGREGACAO(TipoMsg tipo, String produto, int dias) throws IOException{
         Agregacao agregacao = new Agregacao(produto, dias);
         Mensagem mensagem = new Mensagem(idMensagem++, tipo, agregacao.serialize());
         this.send(mensagem);
     }
 
+    /** 
+     * Envia uma mensagem de filtro ao servidor e lança uma thread para aguardar a resposta
+     * 
+     * @param tipo O tipo de mensagem
+     * @param produtos A lista de produtos a filtrar
+     * @param dias O número de dias para filtragem
+     * @throws IOException se ocorrer um erro ao enviar a mensagem
+     */
     public void sendFILTRAR(TipoMsg tipo, List<String> produtos, int dias) throws IOException{
         Filtrar filtrar = new Filtrar(produtos, dias);
         Mensagem mensagem = new Mensagem(idMensagem++, tipo, filtrar.serialize());
         this.send(mensagem);
     }
 
+    /** 
+     * Envia uma mensagem de notificação de vendas consecutivas ao servidor e lança uma thread para aguardar a resposta
+     * 
+     * @param tipo O tipo de mensagem
+     * @param n O número de vendas consecutivas
+     * @throws IOException se ocorrer um erro ao enviar a mensagem
+     */
     public void sendNotificacaoVC(TipoMsg tipo, int n) throws IOException{
         NotificacaoVC notificacao = new NotificacaoVC(n);
         Mensagem mensagem = new Mensagem(idMensagem++, tipo, notificacao.serialize());
         this.send(mensagem);
     }
 
+    /** 
+     * Envia uma mensagem de notificação de vendas simultâneas ao servidor e lança uma thread para aguardar a resposta
+     * 
+     * @param tipo O tipo de mensagem
+     * @param prod1 O primeiro produto
+     * @param prod2 O segundo produto
+     * @throws IOException se ocorrer um erro ao enviar a mensagem
+     */
     public void sendNotificacaoVS(TipoMsg tipo, String prod1, String prod2) throws IOException{
         NotificacaoVS notificacao = new NotificacaoVS(prod1, prod2);
         Mensagem mensagem = new Mensagem(idMensagem++, tipo, notificacao.serialize());
         this.send(mensagem);
     }
 
-    public List<String> getRepliesList() { // o sender pode estar a modificar a lista
+    /** 
+     * Obtém uma cópia da lista de respostas recebidas
+     * 
+     * @return Uma cópia da lista de respostas recebidas
+     */
+    public List<String> getRepliesList() {
         lock.lock(); 
         try {
             return new ArrayList<>(replies);
@@ -155,6 +262,11 @@ public class Stud2 implements AutoCloseable {
         }
     }
 
+    /** 
+     * Obtém uma cópia do mapa de timestamps de mensagens
+     * 
+     * @return Uma cópia do mapa de timestamps de mensagens
+     */
     public Map<TipoMsg, List<Long>> getMessageTimestamps() {
         lock.lock();
         try {
