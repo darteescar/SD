c977dd3ad4c92c3854c2b04da4059b651ca132fa
Duarte Escairo <a106936@alunos.uminho.pt>
2026-01-04 22:33:44 +0000

    MensagemCorrompidaException

--- registoDeEventos/src/entities/MensagemCorrompidaException.java
+++ registoDeEventos/src/entities/MensagemCorrompidaException.java
@@ -2,14 +2,19 @@ package entities;
 
 import java.net.ProtocolException;
 
+/** Exceção lançada quando uma Mensagem está corrompida */
 public class MensagemCorrompidaException extends ProtocolException {
+
+    /** ID da mensagem corrompida */
     private final int id;
 
+    /** Construtor da exceção com o ID da mensagem corrompida e a mensagem de erro */
     public MensagemCorrompidaException(int id, String msg) {
         super(msg);
         this.id = id;
     }
 
+    /** Devolve o ID da mensagem corrompida */
     public int getId() {
         return id;
     }
