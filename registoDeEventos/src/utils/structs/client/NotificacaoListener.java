91ee54a974f406d0fb2ac4f76b7bc05de0e339c3
Duarte Escairo <a106936@alunos.uminho.pt>
2026-01-04 23:35:49 +0000

    NotificacaoListener

--- registoDeEventos/src/utils/structs/client/NotificacaoListener.java
+++ registoDeEventos/src/utils/structs/client/NotificacaoListener.java
@@ -1,7 +1,16 @@
 package utils.structs.client;
 
+/** Interface para controlar as notificações que são enviadas por um cliente */
 public interface NotificacaoListener {
+
+    /** 
+     * Método chamado quando uma notificação de vendas simultâneas é enviada
+     */
     void notificacaoVSEnviada();
+
+    /** 
+     * Método chamado quando uma notificação de vendas consecutivas é enviada
+     */
     void notificacaoVCEnviada();
 }
 
