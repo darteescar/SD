ff36bd5a699824e3d275956e6a5510f7382333b0
Duarte Escairo <a106936@alunos.uminho.pt>
2026-01-04 23:35:24 +0000

    TipoMsg

--- registoDeEventos/src/enums/TipoMsg.java
+++ registoDeEventos/src/enums/TipoMsg.java
@@ -1,17 +1,44 @@
 package enums;
 
+/** Tipo de uma Mensagem */
 public enum TipoMsg {
+
+    /** Mensagem de login */
     LOGIN,
+
+    /** Mensagem de registo de um user */
     REGISTA_LOGIN,
+
+    /** Mensagem de registo de um evento */
     REGISTO,
+
+    /** Mensagem de agregação de eventos */
     QUANTIDADE_VENDAS,
+
+    /** Mensagem de agregação do volume de vendas */
     VOLUME_VENDAS,
+
+    /** Mensagem de agregação do preço médio */
     PRECO_MEDIO,
+
+    /** Mensagem de agregação do preço máximo */
     PRECO_MAXIMO,
+
+    /** Mensagem para o filtro de eventos se uma série */
     LISTA,
+
+    /** Mensagem de notificação de vendas concorrentes */
     NOTIFICACAO_VC,
+
+    /** Mensagem de notificação de vendas simultâneas */
     NOTIFICACAO_VS,
+
+    /** Mensagem de resposta que o servidor envia ao cliente */
     RESPOSTA,
+
+    /** Mensagem que o ServerReader usa para sinalizar o ServerWriter de que o Cliente terminou */
     POISON_PILL,
+
+    /** Mensagem de erro */
     ERRO
 }
