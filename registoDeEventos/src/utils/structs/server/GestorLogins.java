763b41f77d8f39290fc17e1bdc35efb6510770c3
Duarte Escairo <a106936@alunos.uminho.pt>
2026-01-04 23:46:07 +0000

    GestorLogins

--- registoDeEventos/src/utils/structs/server/GestorLogins.java
+++ registoDeEventos/src/utils/structs/server/GestorLogins.java
@@ -1,13 +1,28 @@
 package utils.structs.server;
 import databases.BDUsers;
 
+/** Classe responsável pela gestão dos utilizadores registados */
 public class GestorLogins {
+
+     /** Instância da base de dados de utilizadores */
      BDUsers BDUsers;
 
+     /** 
+      * Construtor da classe GestorLogins. Inicializa a instância da base de dados de utilizadores.
+      * 
+      * @return Uma nova instância de GestorLogins
+      */
      public GestorLogins(){
           this.BDUsers = BDUsers.getInstance();
      }
 
+     /** 
+      * Regista um novo utilizador na base de dados.
+      * 
+      * @param username Nome de utilizador
+      * @param password Palavra-passe do utilizador
+      * @return true se o registo for bem-sucedido, false caso contrário
+      */
      public boolean registar(String username,String password) {
           if (username == null || password == null) {
                return false; // campos inválidos
@@ -19,6 +34,13 @@ public class GestorLogins {
           return false; // user existe
      }
      
+     /** 
+      * Autentica um utilizador com base no nome de utilizador e palavra-passe fornecidos.
+      * 
+      * @param username Nome de utilizador
+      * @param password Palavra-passe do utilizador
+      * @return true se a autenticação for bem-sucedida, false caso contrário
+      */
      public boolean autenticar(String username,String password) {
           if (BDUsers.containsUser(username)){ // se está na BD
                if (BDUsers.get(username).equals(password)) { // se a password guardada é igual à passada
@@ -28,6 +50,13 @@ public class GestorLogins {
           return false; // user nao existe ou password incorreta
      }
 
+     /** 
+      * Apaga um utilizador da base de dados.
+      * 
+      * @param username Nome de utilizador
+      * @param password Palavra-passe do utilizador
+      * @return true se a remoção for bem-sucedida, false caso contrário
+      */
      public boolean apagar(String username, String password) {
           if (BDUsers.containsUser(username)){ // se está na BD
                if (BDUsers.get(username).equals(password)) {  // se a password guardada é igual à passada
