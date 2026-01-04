06f98148f6d523e4d670ccf8b4af21e1085bd7ee
Duarte Escairo <a106936@alunos.uminho.pt>
2026-01-04 22:36:28 +0000

    ServerData

--- registoDeEventos/src/entities/ServerData.java
+++ registoDeEventos/src/entities/ServerData.java
@@ -1,18 +1,40 @@
 package entities;
 
+/** Representa os dados enviados que são processados pelo servidor */
 public class ServerData {
+
+     /** Identificador do cliente */
      private final int clienteID;
+
+     /** Mensagem enviada pelo cliente */
      private final Mensagem mensagem;
 
+     /** 
+      * Construtor da classe ServerData
+      * 
+      * @param clienteID Identificador do cliente
+      * @param mensagem Mensagem enviada pelo cliente
+      * @return Uma nova instância de ServerData
+      */
      public ServerData(int clienteID, Mensagem mensagem) {
           this.clienteID = clienteID;
           this.mensagem = mensagem;
      }
 
+     /** 
+      * Obtém o identificador do cliente
+      * 
+      * @return Identificador do cliente
+      */
      public int getClienteID() {
           return clienteID;
      }
 
+     /** 
+      * Obtém a mensagem enviada pelo cliente
+      * 
+      * @return Mensagem enviada pelo cliente
+      */
      public Mensagem getMensagem() {
           return mensagem;
      }
