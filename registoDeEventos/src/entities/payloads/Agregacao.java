8393155eebf34e7ba26fcf222bb1df4efb1d353c
Duarte Escairo <a106936@alunos.uminho.pt>
2026-01-04 22:46:13 +0000

    Agregacao

--- registoDeEventos/src/entities/payloads/Agregacao.java
+++ registoDeEventos/src/entities/payloads/Agregacao.java
@@ -7,39 +7,84 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ProtocolException;
 
+/** Guarda as informações de uma agregação */
 public class Agregacao {
-    private String produto;
-    private int dias;
-    private static final int MAX_PRODUTO_LENGTH = 1_000; // limite arbitrário
 
+    /** Nome do produto */
+    private String produto;
+
+    /** Número de dias para a agregação */
+    private int dias;
+
+    /** Limite máximo para o comprimento do nome do produto */
+    private static final int MAX_PRODUTO_LENGTH = 1_000;
+
+    /** 
+     * Construtor parametrizado
+     * 
+     * @param produto Nome do produto
+     * @param dias Número de dias para a agregação
+     * @return Uma nova instância de Agregação
+     */
     public Agregacao(String produto, int dias){
         this.produto = produto;
         this.dias = dias;
     }
 
+    /** 
+     * Construtor de cópia
+     * 
+     * @param agregacao Agregação a ser copiada
+     * @return Uma nova instância de Agregação
+     */
     public Agregacao(Agregacao agregacao){
         this.produto = agregacao.getProduto();
         this.dias = agregacao.getDias();
     }
 
+    /** 
+     * Devolve o nome do produto
+     * 
+     * @return Nome do produto
+     */
     public String getProduto(){
         return this.produto;
     }
 
+    /** 
+     * Devolve o número de dias
+     * 
+     * @return Número de dias
+     */
     public int getDias(){
         return this.dias;
     }
 
+    /** 
+     * Representação em String da agregação
+     * 
+     * @return String representando a agregação
+     */
     @Override
     public String toString(){
         return "Produto: " + this.produto + " ,Dias: " + this.dias;
     }
 
+    /** 
+     * Cria uma cópia da agregação
+     * 
+     * @return Cópia da agregação
+     */
     @Override
     public Agregacao clone(){
         return new Agregacao(this);
     }
 
+    /** 
+     * Serializa a agregação em um array de bytes
+     * 
+     * @return Array de bytes representando a agregação
+     */
     public byte[] serialize() {
         try {
             
@@ -58,6 +103,14 @@ public class Agregacao {
         }
     }
 
+    /** 
+     * Desserializa um array de bytes numa agregação
+     * 
+     * @param bytes Array de bytes a ser desserializado
+     * @return Agregação desserializada
+     * @throws IOException Se ocorrer um erro de I/O durante a desserialização
+     * @throws ProtocolException Se os dados estiverem num formato inválido
+     */
     public static Agregacao deserialize(byte[] bytes) throws IOException, ProtocolException {
         if (bytes == null) {
             throw new ProtocolException("Bytes nulos recebidos");
