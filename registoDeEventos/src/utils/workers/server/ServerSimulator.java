2715cc9242943650ea3e10803388b499a7620c7a
Duarte Escairo <a106936@alunos.uminho.pt>
2026-01-05 00:02:49 +0000

    ServerSimulator

--- registoDeEventos/src/utils/workers/server/ServerSimulator.java
+++ registoDeEventos/src/utils/workers/server/ServerSimulator.java
@@ -2,15 +2,30 @@ package utils.workers.server;
 
 import main.Server;
 
+/** Thread responsável por simular a passagem do tempo no servidor */
 public class ServerSimulator implements Runnable{
+
+    /** Server */
     private final Server server;
+
+    /** Intervalo de tempo em milissegundos para simular a passagem de um dia */
     private final long intervalo;
 
+    /** 
+     * Construtor da classe ServerSimulator
+     * 
+     * @param server Server
+     * @param intervalo Intervalo de tempo em milissegundos para simular a passagem de um dia
+     * @return Uma nova instância de ServerSimulator
+     */
     public ServerSimulator(Server server, long intervalo) {
         this.server = server;
         this.intervalo = intervalo;
     }
 
+    /** 
+     * Método run da thread que simula a passagem do tempo no servidor
+     */
     @Override
     public void run(){
         try{
