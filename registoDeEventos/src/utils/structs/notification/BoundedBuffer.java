72c9639ddd2cc8b3e2d1595f138a01b0c575792a
Duarte Escairo <a106936@alunos.uminho.pt>
2026-01-05 00:20:12 +0000

    BoundedBuffer

--- registoDeEventos/src/utils/structs/notification/BoundedBuffer.java
+++ registoDeEventos/src/utils/structs/notification/BoundedBuffer.java
@@ -5,18 +5,34 @@ import java.util.Queue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+/** Buffer genérico sinalizador usado pelo Server */
 public class BoundedBuffer<T> {
 
+    /** Fila para armazenar os itens do buffer */
     private final Queue<T> queue;
+
+    /** Lock para controlo de acesso ao buffer */
     private final ReentrantLock lock;
+
+    /** Condition para controlo de acesso ao buffer */
     private final Condition condition;
 
+    /** 
+     * Construtor do BoundedBuffer. Inicializa a fila, o lock e a condition.
+     * 
+     * @return Uma nova instância de BoundedBuffer
+     */
     public BoundedBuffer() {
         this.queue = new ArrayDeque<>();
         this.lock = new ReentrantLock();
         this.condition = this.lock.newCondition();
     }
 
+    /** 
+     * Adiciona um item ao buffer e sinaliza qualquer thread à espera.
+     * 
+     * @param item O item a ser adicionado ao buffer
+     */
     public void add(T item) {
         lock.lock();
         try {
@@ -27,6 +43,11 @@ public class BoundedBuffer<T> {
         }
     }
 
+    /**
+     * Remove e retorna um item do buffer. Se o buffer estiver vazio, a thread aguarda até que um item esteja disponível.
+     *
+     * @return O item removido do buffer
+     */
     public T poll() {
         lock.lock();
         try {
